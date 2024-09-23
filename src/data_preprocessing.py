62254d49ae5ba06c707a6eca8a57263d6669ae48
Krzysztof Wasik <Krzysztof.Wasik2002@gmail.com>
2024-09-23 23:17:38 +0200

    Update data_preprocessing.py to process MIDI notes with additional features (pitch, velocity, start time, end time) instead of just pitch

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -19,19 +19,25 @@ def load_midi_data(data_path):
 def preprocess_data_chunked(all_midi_notes, sequence_length=50, chunk_size=1000):
     inputs = []
     targets = []
-    
-    for song in all_midi_notes:
-        pitches = [note['pitch'] for note in song]
 
-        for i in range(0, len(pitches) - sequence_length):
-            inputs.append(pitches[i:i+sequence_length])
-            targets.append(pitches[i+sequence_length:i+sequence_length+4])
+    for song in all_midi_notes:
+        song_inputs = []
+        
+        for note in song:
+            song_inputs.append([note['pitch'], note['velocity'], note['start'], note['end']])
+        
+        for i in range(0, len(song_inputs) - sequence_length):
+            sequence_input = song_inputs[i:i + sequence_length]
+            sequence_target = song_inputs[i + sequence_length:i + sequence_length + 4]
+            
+            inputs.append(sequence_input)
+            targets.append(sequence_target)
         
         if len(inputs) >= chunk_size:
             save_preprocessed_chunk(inputs, targets)
             inputs = []
             targets = []
-    
+
     if inputs:
         save_preprocessed_chunk(inputs, targets)
 
@@ -39,7 +45,6 @@ def save_preprocessed_chunk(inputs, targets):
     with open(processed_data_path, 'ab') as f:
         pickle.dump((inputs, targets), f)
     print("Saved a chunk of preprocessed data")
-
 
 all_midi_notes = load_midi_data(data_path)
 
